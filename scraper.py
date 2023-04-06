dda701f38f86e2f346b2a2fb2386c9b5aaab291c
Artem Omelchenko <56344591+sens0rer@users.noreply.github.com>
2023-04-06 11:57:09 +0300

    A function to get steam price history for items

--- scraper.py
+++ scraper.py
@@ -3,11 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 from io import StringIO
 from pandas import DataFrame
+import urllib.parse
+import datetime as dt
 
-def get_playercount(appid: int=730)->dict:
+
+def get_playercount(appid: int = 730) -> dict:
     """
     Extracts current playercount from Steam API and 24-hour and all-time peak from steamcharts.com
-    
+
     Parameters:
         appid (int): Steam appid. Defaults to 730 (CS:GO)
     Returns:
@@ -16,14 +19,17 @@ def get_playercount(appid: int=730)->dict:
     page = requests.get(
         f'https://steamcharts.com/app/{appid}')
     soup = BeautifulSoup(page.content, 'lxml')
-    tag = soup.find('div',id='app-heading')
-    tags = tag.find_all('div',class_="app-stat")
+    tag = soup.find('div', id='app-heading')
+    tags = tag.find_all('div', class_="app-stat")
     result = {}
     result['24-hour peak'] = int(tags[1].find('span').string)
     result['All-time peak'] = int(tags[2].find('span').string)
-    response = requests.get(f'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}')
-    result['Playercount'] = response.json().get('response',{}).get('player_count',0)
+    response = requests.get(
+        f'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}')
+    result['Playercount'] = response.json().get(
+        'response', {}).get('player_count', 0)
     return result
+
 
 def get_unboxing_numbers() -> DataFrame:
     """
@@ -32,13 +38,17 @@ def get_unboxing_numbers() -> DataFrame:
     Returns:
         DataFrame: A dataframe with total, monthly, weekly and daily unboxing number for every case
     """
-    daily_response = requests.get('https://csgocasetracker.com/calculations/calcDaily.csv')
+    daily_response = requests.get(
+        'https://csgocasetracker.com/calculations/calcDaily.csv')
     daily_csv = StringIO(daily_response.content.decode('utf-8'))
-    weekly_response = requests.get('https://csgocasetracker.com/calculations/calcWeekly.csv')
+    weekly_response = requests.get(
+        'https://csgocasetracker.com/calculations/calcWeekly.csv')
     weekly_csv = StringIO(weekly_response.content.decode('utf-8'))
-    monthly_response = requests.get('https://csgocasetracker.com/calculations/calculation.csv')
+    monthly_response = requests.get(
+        'https://csgocasetracker.com/calculations/calculation.csv')
     monthly_csv = StringIO(monthly_response.content.decode('utf-8'))
-    total_response = requests.get('https://csgocasetracker.com/calculations/calculationTotal.csv')
+    total_response = requests.get(
+        'https://csgocasetracker.com/calculations/calculationTotal.csv')
     total_csv = StringIO(total_response.content.decode('utf-8'))
     daily_df = pd.read_csv(daily_csv, sep=',')
     weekly_df = pd.read_csv(weekly_csv, sep=',')
@@ -51,3 +61,60 @@ def get_unboxing_numbers() -> DataFrame:
     result['Weekly Unboxing Number'] = weekly_df['Unboxing Number']
     result['Daily Unboxing Number'] = daily_df['Unboxing Number']
     return result
+
+
+def get_price_history(item_name: str) -> DataFrame:
+    """
+    Extracts price history for an item from Steam
+
+    Returns:
+        DataFrame: A dataframe with price history containing datetimes, prices and amount sold for the given item
+    """
+
+    url = f"https://steamcommunity.com/market/listings/730/{urllib.parse.quote(item_name)}"
+    response = requests.get(url).text
+    response = response[response.find("line1")+6:]
+    response = response[0:response.find("]];")+2]
+
+    price_list = eval(response)
+    month_list = [None,
+                  'Jan',
+                  'Feb',
+                  'Mar',
+                  'Apr',
+                  'May',
+                  'Jun',
+                  'Jul',
+                  'Aug',
+                  'Sep',
+                  'Oct',
+                  'Nov',
+                  'Dec']
+    for i, entry in enumerate(price_list):
+        # Date and time
+        date_time = entry[0]
+        date_time = date_time.split(" ")
+        month = month_list.index(date_time[0])
+        day = int(date_time[1])
+        year = int(date_time[2])
+        hour = int(date_time[3].split(":")[0])
+        date_time = dt.datetime(
+            year, month, day, hour=hour, minute=0, second=0)
+
+        # Price
+        price = float(entry[1])
+
+        # Amount sold
+        sold = int(entry[2])
+
+        # compile dictionary and add it back to the list
+        price_dict = {
+            "Date": date_time,
+            "Price(USD)": price,
+            "Amount sold": sold
+        }
+        price_list[i] = price_dict
+
+    df = pd.DataFrame.from_dict(price_list)
+
+    return df
